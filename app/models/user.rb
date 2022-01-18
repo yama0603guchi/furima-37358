f621a552bdda86f5a67a66f8a85fa7a545cbcd89
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-18 19:14:18 +0900

    バリデーションと新規登録画面まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
+  validates :first_name, 
+  format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+  presence: true
+  validates :family_name, 
+  format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+  presence: true
+  validates :first_name_kana,
+  format: { with: /\A([ァ-ン]|ー)+\z/},
+   presence: true
+  validates :family_name_kana,
+  format: { with: /\A([ァ-ン]|ー)+\z/},
+   presence: true
   validates :birth_day, presence: true
 
 end
