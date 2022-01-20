153c0b61b2eb59c8e1ae108011b5adf66cb0bdb2
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-20 18:23:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage_area
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :preparation_day
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: {message: "can't be blank" }
+  end
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :postage_player_id, presence:true
+  validates :postage_area_id, presence: true
+  validates :preparation_day_id, presence: true
+  validates :image, presence: true
 end
