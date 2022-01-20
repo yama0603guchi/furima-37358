57b42087c7b20ec8d355ae43a291b2768a80d53c
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-20 17:08:12 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_condition
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :postage_player
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :postage_area
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :preparation_day
 end
---
+++ spec/factories/postage_areas.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :postage_area do
+    
+  end
+end
