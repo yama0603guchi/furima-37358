23987fb99f5ecdb9822ec043443565f73dbee6b3
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-20 18:56:35 +0900

    出品した商品の情報をテーブルに保存できるようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except [:index]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
 
-  def message_params
-    params.require(:item).permit(:name, :explanation, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :explanation, :image, :price).merge(user_id: current_user.id)
   end
 end
