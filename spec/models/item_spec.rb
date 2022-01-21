441db5037a904f351d753f8d81f16a5341f948f7
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-21 17:37:24 +0900

    itemモデルテストコード成功

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:iten)
+    @item = FactoryBot.build(:item)
   end
 
   describe '商品出品機能' do
     context '商品の出品がうまくいく時' do
-      it '全ての値が入力されていれば出品できる'
+      it '全ての値が入力されていれば出品できる' do
         expect(@item).to be_valid
       end
       it 'priceが半角数値であれば出品できる' do
@@ -38,27 +38,27 @@ RSpec.describe Item, type: :model do
       it 'category_idが未選択だと出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Category id can't be blank"
+        expect(@item.errors.full_messages).to include "Category can't be blank"
       end
       it 'item_condition_idが未選択だと出品できない' do
         @item.item_condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Item condition id  can't be blank"
+        expect(@item.errors.full_messages).to include "Item condition can't be blank"
       end
       it 'postage_player_idが未選択だと出品できない' do
         @item.postage_player_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Postage player id can't be blank"
+        expect(@item.errors.full_messages).to include "Postage player can't be blank"
       end
       it 'postage_area_idが未選択だと出品できない' do
         @item.postage_area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Postage area id can't be blank"
+        expect(@item.errors.full_messages).to include "Postage area can't be blank"
       end
       it 'preparation_day_idが未選択だと出品できない' do
         @item.preparation_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Preparation day id can't be blank"
+        expect(@item.errors.full_messages).to include "Preparation day can't be blank"
       end
       it '販売価格が¥300より少ない時は出品できない' do
         @item.price = 299
@@ -68,7 +68,7 @@ RSpec.describe Item, type: :model do
       it '販売価格が¥9999999より多い時は出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than or to 9999999')
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it 'priceが空だと出品できない' do
         @item.price = ''
@@ -76,7 +76,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
       it 'imageが空だと出品できない' do
-        @item.name = ''
+        @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include "Image can't be blank"
       end
