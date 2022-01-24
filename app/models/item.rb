71e2ce7af708a4bd936162da7b821746768af911
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-24 17:34:43 +0900

    再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :preparation_day
 
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "can't be blank" }
-  end
+  validates :price,
+   format: { with: /\A[0-9]+\z/ },
+   numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+   presence: true
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,6 +70,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it 'priceが空だと出品できない' do
         @item.price = ''
         @item.valid?
