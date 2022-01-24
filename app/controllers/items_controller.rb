c97fdb226f5b0cb68660db30ac0b1e8f54e1522b
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-24 18:00:49 +0900

    再修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @items = Item.all
+    #@items = Item.all
   end
 
   def new
-    #@item = Item.new
+    @item = Item.new
   end
 
   def create
