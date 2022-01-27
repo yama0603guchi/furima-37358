bf2b9ae04805a754dc115b0c42278e068462fbf0
yama0603guchi <yamaguchi0603@icloud.com>
2022-01-27 18:08:15 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item =Item.find(params[:id])
+    item = Item.find(params[:id])
     item.destroy
     redirect_to root_path
   end
