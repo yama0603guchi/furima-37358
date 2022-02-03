e688ad7678df01e615a6a7dbbe5af2b802b71ace
yama0603guchi <yamaguchi0603@icloud.com>
2022-02-03 18:11:32 +0900

    Formオブジェクトのテストコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,12 +6,12 @@ class OrderAddress
   #belongs_to_active_hash :postage_area
   validates :postage_area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :post_code,
-            format: { with: /\A\d{3}[-]\d{4}\z/, message: '3桁ハイフン4桁の半角文字列を使用してください'},
+            format: { with: /\A\d{3}[-]\d{4}\z/},
             presence: true
   validates :municipalities, presence: true
   validates :street_number, presence: true
   validates :telephone_number,
-            format: { with: /\A\d{10,11}\z/, message: '10桁以上11桁以内の半角数値を使用してください'},
+            format: { with: /\A\d{10,11}\z/},
             presence: true
   validates :user_id, presence: true
   validates :item_id, presence: true
---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code           {'123-4567'}
+    postage_area_id     { 24 }
+    municipalities      {'名古屋'}
+    street_number       {'1-1'}
+    building_name       {'名古屋ハイツ'}
+    telephone_number    { 12345678912 }
+  end
+end
